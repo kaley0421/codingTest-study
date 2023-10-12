e909582118641fd7f402fb6f7ccf348839be6f15
jin800812 <jin800812@gmail.com>
2023-10-12 15:57:40 +0900

    Update 11055_jsi.py

--- 5주차_dp/[BOJ 11055] 가장 큰 증가하는 부분수열/11055_jsi.py
+++ 5주차_dp/[BOJ 11055] 가장 큰 증가하는 부분수열/11055_jsi.py
@@ -1,13 +1,12 @@
+# DP[x] = x번을 포함한 x번까지 증가하는 수열 최대 합
+
 N = int(input())
 A = list(map(int, input().split()))
-DP = [0] * N
-DP[0] = A[0]
+DP = [i for i in A]
 
-for i in range(1, N):
+for i in range(N):
     for j in range(i):
         if A[j] < A[i]:
             DP[i] = max(DP[i], DP[j] + A[i])
-        else:
-            DP[i] = max(DP[i], A[i])
-        print((i, j), DP)
+print(DP)
 print(max(DP))
